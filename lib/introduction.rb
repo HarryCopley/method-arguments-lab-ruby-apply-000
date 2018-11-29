0248830f609be5967e7fccd5d923ba05ef8bb1a6
generous-program-0200 <harrycopley@vultuslimited.com>
2018-11-29 15:00:42 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,8 @@
+def method(x)
+  puts "#{x} my name is harry"
+end
+
+def introduction_with_language(name, language)
+  puts "Hi my name is #{name} and I am learning to program in #{language}"
+end
+
